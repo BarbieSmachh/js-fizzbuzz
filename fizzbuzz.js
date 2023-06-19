7b113c46d6ce38e32c3bf8c2513f01702839f8f5
BarbieSmachh <hicham.-sebbar@hotmail.fr>
2023-06-19 15:52:13 +0200

    boucle

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,33 +5,53 @@
 // expected here.
 
 'use strict';
-console.log('v1')
-var fizzbuzz = function (x) {
-if((x%3===0)&&(x%5===0)){
-    console.log('fizzbuzz');
-};
-if((x%3===0)&&(x%5!=0)){
-    console.log('fizz');
-};
-if((x%5===0)&&(x%3!=0)){
-    console.log('buzz');
-};
-if((x%3!=0)&&(x%5!=0)){
-    console.log(x);
-};
-};
-fizzbuzz(15);
-console.log('v2')
-var fizzbuzz = function (x) {
-    if((x%3===0)&&(x%5===0)){
-        console.log('fizzbuzz');
-       }else if((x%3!=0)&&(x%5!=0)){
-        console.log(x);
-    }else if((x%5===0)&&(x%3!=0)){
-        console.log('buzz');
-    }else if((x%3===0)&&(x%5!=0)){
-        console.log('fizz');
-    };
+// console.log('v1')
+// var fizzbuzz = function (x) {
+// if((x%3===0)&&(x%5===0)){
+//     console.log('fizzbuzz');
+// };
+// if((x%3===0)&&(x%5!==0)){
+//     console.log('fizz');
+// };
+// if((x%5===0)&&(x%3!==0)){
+//     console.log('buzz');
+// };
+// if((x%3!=0)&&(x%5!==0)){
+//     console.log(x);
+// };
+// };
+// fizzbuzz(15);
+// console.log('v2')
+// var fizzbuzz = function (x) {
+//     if((x%3===0)&&(x%5===0)){
+//         console.log('fizzbuzz');
+//     }else if((x%3===0)&&(x%5!==0)){
+//         console.log('fizz')
+//     }else if((x%5===0)&&(x%3!==0)){
+//         console.log('buzz');
+//     }else{
+//         console.log(x);
+//     };
     
+// }
+// fizzbuzz(15);
+
+// for (let c = 0; c<11; c++){
+//     console.log(c);
+// };
+// let tablec = [0,1,2,3,4,5,6,7,8,9,10]
+// tablec.forEach(chiffre => {
+//     console.log(chiffre);
+// })
+let  alphabetFrancais = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 's', 'm', 'n', 'o', 'p', 'q', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+let a=''
+let i = -1
+while (a!=='s'){
+    if (a==='s'){
+        break;
+    }else{ i++, a = alphabetFrancais[i]; 
+        console.log(a,i+1);
 }
-fizzbuzz(15);
+};
